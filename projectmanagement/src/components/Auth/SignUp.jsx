2be104012fa57bec78a6a8ee500c3f3b697199d2
ranime-sb <ranime.sboui@esprit.tn>
2025-05-06 22:52:13 +0200

    cicd

--- projectmanagement/src/components/Auth/SignUp.jsx
+++ projectmanagement/src/components/Auth/SignUp.jsx
@@ -28,7 +28,7 @@ const SignUp = () => {
     console.log("Form Data:", formData);
 
     try {
-      const apiBaseUrl = process.env.REACT_APP_API_BASE_URL || "http://localhost:4000/api/auth"; // Fallback local
+      const apiBaseUrl = process.env.REACT_APP_API_URL || "http://localhost:4000/api/auth"; // Fallback local
       const response = await fetch(`${apiBaseUrl}/register`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
